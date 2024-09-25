96a1449cae297d3074175db2e674be2f3793a9dd
AmeeYuki <yukinooame@gmail.com>
2024-09-25 18:49:14 +0700

    update carousel

--- src/pages/productDetail/components/CarouselImage.jsx
+++ src/pages/productDetail/components/CarouselImage.jsx
@@ -1,14 +1,15 @@
+import { Carousel } from "antd";
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function CarouselImage() {
   return (
     <div>
       <Carousel
-        showArrows={true}
-        autoPlay={true}
-        dynamicHeight={false}
+        arrows
+        infinite={false}
+        // showArrows={true}
+        // autoPlay={true}
+        // dynamicHeight={false}
         // centerMode={true}
         // onChange={onChange}
         // onClickItem={onClickItem}
